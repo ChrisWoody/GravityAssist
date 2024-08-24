831786b64ca00f2a69eaf0f43906beafb2972ced
Chris Wood <16053164+ChrisWoody@users.noreply.github.com>
2024-08-24 10:49:45 +0800

    Play around with gravity effect

--- scripts/planet.gd
+++ scripts/planet.gd
@@ -10,11 +10,13 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	var diff := position - spaceship.position
-	if diff.length() < 150.0:
-		var gravityRotation := -2.0 if spaceship.position.y > position.y else 2.0
-		spaceship.applyGravity(diff.normalized(), gravityRotation)
+	if diff.length() < 150.0:		
+		var forward := Vector2.from_angle(deg_to_rad(spaceship.rotation_degrees - 90.0)).normalized()
+		var angleToPlanet := rad_to_deg(diff.angle_to(forward))
+		var angleToUse := -1.0 if angleToPlanet > 0.0 else 1.0
+		var scaleToUse := -(diff.length() / 150.0) + 1
 
-	pass
+		spaceship.applyGravity(Vector2.ZERO, angleToUse * scaleToUse * scaleToUse)
 
 
 func _on_area_2d_area_entered(area:Area2D) -> void:
